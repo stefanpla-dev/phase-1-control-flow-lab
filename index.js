a1b181cd63e6bda928f055c1db594e10b7042069
Stefan Pla <spla@icloud.com>
2024-01-29 15:59:26 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  if (distance >= 400 && distance <= 2000) {
+    return "That will be twenty bucks."
+  } else if (distance < 400) {
+  return "This one is on me!";
+} else if (distance > 2000 && distance <= 2500) {
+  return "I will gladly take your thirty bucks.";
+} else if (distance >= 2500){
+  return "No can do.";
+}
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(generosity){
+  if (generosity === "generous") {
+    return "Thank you so much.";
+  } else if (generosity === "not as generous") {
+    return "Thank you.";
+  } else {
+    return "Bye.";
+  }
 }
